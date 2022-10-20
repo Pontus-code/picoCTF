5ddf820887d8289b0b135fc9e2fe3aa4c5b6e5c6
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-10-21 00:26:27 +0200

    Update and rename picoCTF_Nice_netcat... to picoCTF_Nice_netcat...py

--- picoCTF_Nice_netcat...py
+++ picoCTF_Nice_netcat...py
@@ -9,7 +9,7 @@
 # There is a nice program that you can talk to by using this command in a shell: $ nc mercury.picoctf.net 7449, but it doesn't speak English...
 
 # Connect and you will receive numbers. Output them to a file.
-nc mercury.picoctf.net 7449 > numbers.txt
+# nc mercury.picoctf.net 7449 > numbers.txt
 
 # We have text file with numbers, one number for each row. 
 # We assume each number represent an ASCII character. 
@@ -27,8 +27,8 @@ list = contents.split('\n')
 list.remove('')
 
 # Converts every entry in the list from a string to an integer, then converts the integer into it's corresponding ASCII character and prints the results.
-for i in list:
-    print(chr(int(i)))
+for number in list:
+    print(chr(int(number)))
 
 # The flag is: picoCTF{g00d_k1tty!_n1c3_k1tty!_f2d7cafa}
 
