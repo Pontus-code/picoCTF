d6c56d262cd2d69d4f5514c6a1155f16b0190f89
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-11-20 14:49:06 +0100

    Update picoCTF_Nice_netcat...py

--- picoCTF_Nice_netcat...py
+++ picoCTF_Nice_netcat...py
@@ -8,27 +8,38 @@
 # Description:
 # There is a nice program that you can talk to by using this command in a shell: $ nc mercury.picoctf.net 7449, but it doesn't speak English...
 
-# Connect and you will receive numbers. Output them to a file.
-# nc mercury.picoctf.net 7449 > numbers.txt
+# Imports the socket module.
+import socket
 
-# We have text file with numbers, one number for each row. 
+host = "mercury.picoctf.net"
+port = 7449
+
+print(f"Connecting to {host}:{port}...")
+# Creates a TCP, IPv4 socket object.
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Connect to the host.
+client.connect((host, port))
+# Receives the data as a byte object.
+print("Receiving data...")
+numbers = client.recv(4096)
+# Converts the byte object to a string.
+numbers = numbers.decode("UTF-8")
+
+# We have string with numbers, one number for each row. 
 # We assume each number represent an ASCII character. 
-# Let's write a python script to convert the numbers into characters!
+# Let's convert the numbers into characters.
 
-# Opens the file and reads its contents to a string variable.
-with open(numbers.txt) as f:
-    contents = f.read()
-
-# Separates the numbers and assigns them to a list of strins.
-list = contents.split('\n')
+# Separates the numbers and assigns them to a list of strings.
+list = numbers.split('\n')
 
 # The last entry in the list is an empty string. It  will cause an error when being converted into an integer later.
 # Removes the empty string from the list.
 list.remove('')
-
+# Prints the numbers received.
+print(''.join(list))
 # Converts every entry in the list from a string to an integer, then converts the integer into it's corresponding ASCII character and prints the results.
-for number in list:
-    print(chr(int(number)))
+print("Converting numbers into corresponding ASCII characters...")
+for integer in list:
+    print(chr(int(integer)), end = '')
 
-# The flag is: picoCTF{g00d_k1tty!_n1c3_k1tty!_f2d7cafa}
 
