124f1221dcae6b2d0bcd8ede028a55cdb039720e
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-11-21 14:06:05 +0100

    Add files via upload

--- picoCTF_First_Grep.py
+++ picoCTF_First_Grep.py
@@ -15,13 +15,14 @@ import re
 
 # URL for the file in the description.
 url = "https://jupiter.challenges.picoctf.org/static/495d43ee4a2b9f345a4307d053b4d88d/file"
+filename = ''.join(re.findall("\w*$", url))
 
 # Requests the file.
-print(f"Downloading {url}...")
+print(f"Downloading {filename}...")
 response = requests.get(url)
 
 print("Searching for the flag using regular expressions... picoCTF{.*}")
-flag = re.findall("picoCTF{.*}", response.content.decode("UTF-8"))
+flag = re.findall("picoCTF{.*}", response.content.decode())
 
 # Prints the flag.
 print("The flag is: " + ''.join(flag))
