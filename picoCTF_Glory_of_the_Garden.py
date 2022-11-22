6726316dcfec875c30d23ff0d844438f240bc0e4
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-11-22 13:18:56 +0100

    Update picoCTF_Glory_of_the_Garden.py

--- picoCTF_Glory_of_the_Garden.py
+++ picoCTF_Glory_of_the_Garden.py
@@ -8,15 +8,27 @@
 # Description: This garden contains more than it seems.
 # Hint: What is a hex editor?
 
-# URL to a file.
-url = "https://jupiter.challenges.picoctf.org/static/43c4743b3946f427e883f6b286f47467/garden.jpg"
-
 import re
 import requests
+from PIL import Image
 
-filename = ''.join(re.findall("\w*\.\w*$", url))
-print(f"Downloading {filename}")
+
+
+url = "https://jupiter.challenges.picoctf.org/static/43c4743b3946f427e883f6b286f47467/garden.jpg" # URL to a file.
+filename = ''.join(re.findall('(\w*\.\w*$)', url)) # Regular expression to match the filename.
+
+def showimage(): # Downloads and displays the image file in the default image viewer.
+    print(f"Downloading {filename} from {url}...")
+    response = requests.get(url, stream=True)
+    print(f"Opening {filename} in the default image viewer...")
+    img = Image.open(response.raw)
+    img.show()
+
+
+showimage()
+
+print("No clue in the picture...")
 file = requests.get(url)
-print(f"Scanning {filename} for the flag...")
+print(f"Lets scan the {filename} file for the flag using regular expressions...")
 flag = re.findall("picoCTF{.*}", file.text)
 print(''.join(flag))
